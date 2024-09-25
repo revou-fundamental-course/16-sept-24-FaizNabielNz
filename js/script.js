3a3ef9d66ff07e6a0112f1a2594cd7002f4863df
FaizNabielNz <faisnabiel@gmail.com>
2024-09-25 22:52:54 +0700

    Konfigurasi Keenam

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-//ini javascript
+replaceName();
+
+function replaceName() {
+  let name = prompt("Kenalan Dong..!!");
+  console.log(name);
+  document.getElementById("user-name").innerHTML = name;
+}
+
+let indexSlide = 1;
+showSlide(1);
+
+function nextSlide(n) {
+  showSlide((indexSlide += n));
+}
+
+//get DOM all Image Banner
+function showSlide(n) {
+  let listImage = document.getElementsByClassName("photo-banner");
+
+  //Reset indexSlide
+  if (n > listImage.length) indexSlide = 1;
+
+  //Hide all images
+  let i = 0;
+  while (i < listImage.length) {
+    listImage[i].style.display = "none";
+    i++;
+  }
+
+  //show selected image
+  listImage[indexSlide - 1].style.display = "block";
+  console.log(listImage);
+}
+
+//Auto run
+setInterval(() => nextSlide(1), 2500);
